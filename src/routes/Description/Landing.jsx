ca7ee20f98c83e782fa1d727a75c93402cf3ca6a
unknown <doaa>
2024-09-02 22:01:51 +0300

    page2

--- src/routes/Description/Landing.jsx
+++ src/routes/Description/Landing.jsx
@@ -2,11 +2,11 @@
 const Landing = () => {
   return (
     <div className="bg-[#0C5685] ">
-        <div className="container p-5 mx-auto flex md:flex-row flex-col-reverse justify-between items-center gap-10">
-            <img src="/Frame454.png" className="md:w-[50%] md:h-[250px] h-[200px]" />
-                <div className=" md:text-end md:w-[40%]   text-center font-bold mt-6 ">
-                    <p className="text-[#FCD275] lg:text-[19px] text-[17px]">  صفة المسرد</p>
-                    <h1 className="  lg:text-[28px] md:text-[20px] text-[22px]  text-[white]">مكان يجمع بين صيغة المتحف والكتاب  <span className="text-[#FCD275]">الذكاء الاصطناعى</span></h1>
+        <div className="container p-5  mx-auto flex md:flex-row flex-col-reverse justify-between items-center gap-10 ">
+            <img src="/Frame454.png" className="md:w-[60%]  w-full  " />
+                <div className=" md:text-end md:w-[40%] w-full   text-center font-bold my-6 flex flex-col gap-4">
+                    <p className="text-[#FCD275] lg:text-[19px] text-[20px]">  صفة المسرد</p>
+                    <h1 className="  lg:text-[28px] md:text-[20px] text-[18px]  text-[white]">مكان يجمع بين صيغة المتحف والكتاب  <span className="text-[#FCD275]">الذكاء الاصطناعى</span></h1>
                 </div>
         </div>
 
--- src/routes/Description/Relace.jsx
+++ src/routes/Description/Relace.jsx
@@ -3,22 +3,21 @@ const Relace = () => {
   return (
         <div className="bg-[#0C5685]">
         <div className=" container  mx-auto   flex flex-col-reverse justify-between items-center md:flex-row  gap-10 rounded-3xl px-5 py-10 ">
-            <img src="/Vector (8).png" className="md:w-[50%] h-[300px] sm:h-[350px]  " />
-            <div className=" md:text-end  flex flex-col gap-5 justify-center md:items-end items-center text-center font-bold lg:w-[60%] md:w-[50%] ">
-                <p className="text-[#FCD275] lg:text-[19px] text-[17px]">    أين يقع المسرد؟</p>
-                <h1 className=" lg:text-[28px] md:text-[20px] text-[22px] text-[white] leading-10">     اعتمادا على كوادر بشرية وتقنية يجمع الفريق <span className="text-[#FCD275]">المعلومات المتاحة</span> عن عائلتكم والمتاحة في الاعلام الرسمى وغير الرسمي. </h1>
-                <div className=" flex flex-col-reverse md:flex-row gap-4 font-bold text-[19px] text-[white] justify-center items-center">
-                    
+            <img src="/Vector (8).png" className="lg:w-[40%] md:w-[50%] w-full  " />
+            <div className=" md:text-end  flex flex-col gap-5 justify-center md:items-end items-center text-center font-bold lg:w-[60%] md:w-[50%] w-full ">
+                <p className="text-[#FCD275] lg:text-[19px] text-[20px]">    أين يقع المسرد؟</p>
+                <h1 className=" lg:text-[28px] md:text-[20px] text-[18px] text-[white] leading-10">     اعتمادا على كوادر بشرية وتقنية يجمع الفريق <span className="text-[#FCD275]">المعلومات المتاحة</span> عن عائلتكم والمتاحة في الاعلام الرسمى وغير الرسمي </h1>
+                <div className=" flex flex-col-reverse md:flex-row gap-4 font-bold text-[16px] md:text-[18px] lg:text-[19px]  text-[white] justify-center items-center">
                     <p>في دار ضيافة العائلة</p>
-                    <div className="bg-[#fcd27533] w-16 h-16 rounded-xl  flex justify-center items-center "><img className="w-10 h-10 " src="/book2.png" /></div>
+                    <div className="bg-[#fcd27533] sm:w-16 sm:h-16 w-14 h-14 rounded-xl  flex justify-center items-center "><img className="w-8 h-8 sm:w-10 sm:h-10 " src="/book2.png" /></div>
                 </div>
-                <div className=" flex flex-col-reverse md:flex-row gap-4 font-bold text-[19px] text-[white]  justify-center items-center">
+                <div className=" flex flex-col-reverse md:flex-row gap-4 font-bold text-[16px] md:text-[18px] lg:text-[19px]  text-[white]  justify-center items-center">
                     <p>  أو مقر الشركة القابضة</p>
-                    <div className="bg-[rgba(252,210,117,0.2)] w-16 h-16 rounded-xl  flex justify-center items-center"><img className="w-10 h-10 " src="/book2.png" /></div>
+                    <div className="bg-[rgba(252,210,117,0.2)] sm:w-16 sm:h-16 w-14 h-14 rounded-xl  flex justify-center items-center"><img className="w-8 h-8 sm:w-10 sm:h-10 " src="/book2.png" /></div>
                 </div>
-                <div className=" flex flex-col-reverse md:flex-row gap-4 font-bold text-[19px] text-[white] justify-center items-center">
+                <div className=" flex flex-col-reverse md:flex-row gap-4 font-bold text-[16px] md:text-[18px] lg:text-[19px]  text-[white] justify-center items-center ">
                     <p>أو يتم بناء مسرد على أرض جديدة تصبح ايقونة التواصل بين العائلة والمجتمع</p>
-                    <div className="bg-[rgba(252,210,117,0.2)] w-16 h-16 rounded-xl  flex justify-center items-center"><img className="w-10 h-10 " src="/book2.png" /></div>
+                    <div className="bg-[rgba(252,210,117,0.2)] sm:w-16 sm:h-16 w-14 h-14 rounded-xl  flex justify-center items-center"><img className="w-8 h-8 sm:w-10 sm:h-10 " src="/book2.png" /></div>
                 </div>
             </div>
         </div>
